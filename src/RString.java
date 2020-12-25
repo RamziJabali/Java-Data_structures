2e55ee526bbfc38738a22f265afcea88c94298c6
Ramzi Jabali <ramzieljabali@gmail.com>
2020-12-25 01:07:17 -0700

    fixed constructors with array domain

--- src/RString.java
+++ src/RString.java
@@ -1,20 +1,28 @@
 public class RString {
     private RChar[] rString;
 
+    public RChar[] getrString() {
+        return rString;
+    }
+
     public RString(RChar rChar) {
         rString = new RChar[1];
         rString[0] = rChar;
     }
 
     public RString(RChar[] rChars) {
-        rString = new RChar[rChars.length + 1];
+        rString = new RChar[rChars.length];
     }
 
     public RString(char character) {
-
+        rString = new RChar[1];
+        rString[0] = new RChar(character);
     }
 
     public RString(String word) {
-
+        rString = new RChar[word.length()];
+        for (int i = 0; i < word.length(); i++) {
+            rString[i] = new RChar(word.charAt(i));
+        }
     }
 }
--- src/Tester.java
+++ src/Tester.java
@@ -2,9 +2,19 @@ public class Tester {
     public static void main(String[] args) {
         RChar test = new RChar(65);
         RChar test2 = new RChar(97);
-        System.out.println("Is this character a digit " + test.isDigit());
-        System.out.println("Is this character a Letter " + test.isLetter());
-        System.out.println("Give me ASCII of 97(a) to uppercase " + test.toUpperCase());
-        System.out.println("Give me ASCII of 65(A) to lowercase " + test2.toLowerCase());
+        RChar Cap[] = new RChar[10];
+//        System.out.println("Is this character a digit " + test.isDigit());
+//        System.out.println("Is this character a Letter " + test.isLetter());
+//        System.out.println("Give me ASCII of 97(a) to uppercase " + test.toUpperCase());
+//        System.out.println("Give me ASCII of 65(A) to lowercase " + test2.toLowerCase());
+//        System.out.println("size of array " + Cap.length);
+//        for (int i = 0; i <= Cap.length; i++) {
+//            Cap[i] = new RChar(65 + i);
+//        }
+        RString string = new RString("ramzi");
+        RChar word[] = string.getrString();
+        for (int j = 0; j < word.length; j++) {
+            System.out.print(word[j]);
+        }
     }
 }
