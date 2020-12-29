455864655630697d44fe6acad5fab716b4849f5b
RamziJabali <ramzieljabali@gmail.com>
2020-12-29 01:46:29 -0700

    Finished add(), Finished toString(), Finished getListSize()

--- src/RArrayList.java
+++ src/RArrayList.java
@@ -1,20 +1,81 @@
-public class RArrayList {
-    private Node next, previous;
-    private Object data;
+import com.sun.xml.internal.bind.v2.schemagen.xmlschema.NoFixedFacet;
 
-    public RArrayList(Object data, Node next, Node previous) {
-        this.data = data;
-        this.next = next;
-        this.previous = previous;
+public class RArrayList<T> {
+    private Node head;
+    private int listSize = 0;
+
+    public RArrayList() {
+        head = null;
+    }
+
+    public int getListSize() {
+        return listSize;
+    }
+
+    public void add(T data) {
+        listSize++;
+        Node node = new Node(data);
+        if (isEmpty(head)) {
+            head = node;
+            return;
+        }
+        if (isEmpty(head.next)) {
+            head.next = node;
+            return;
+        }
+        Node newNode = add(head.next);
+        newNode.next = node;
+    }
+
+    private Node add(Node nextNode) {
+        Node previousNode = null;
+        while (!isEmpty(nextNode)) {
+            previousNode = nextNode;
+            nextNode = nextNode.next;
+        }
+        return previousNode;
+    }
+
+    private boolean isEmpty(Node node) {
+        return node == null;
+    }
+
+    @Override
+    public String toString() {
+        String linkedList = "[";
+        Node node = head;
+        while (node != null) {
+            linkedList += (node);
+            linkedList += ",";
+            node = node.next;
+        }
+        int lastComma = linkedList.lastIndexOf(",");
+        if (lastComma != -1) {
+            linkedList = linkedList.substring(0, linkedList.length() - 1);
+        }
+        linkedList += ']';
+
+        return linkedList;
     }
 
     class Node {
+        T data;
+        Node next;
 
+        public Node(T data) {
+            this.data = data;
+            next = null;
+        }
 
+        public Node(T data, Node next) {
+            this.next = next;
+            this.data = data;
+        }
 
-
-        public Node() {
-
+        @Override
+        public String toString() {
+            return "" + data;
         }
     }
+
 }
--- src/Tester.java
+++ src/Tester.java
@@ -1,26 +1,36 @@
-import javax.rmi.ssl.SslRMIClientSocketFactory;
-
 public class Tester {
     public static void main(String[] args) {
-        //System.out.print("MOE");
-//        RChar test = new RChar('s');
-//        RChar test2 = new RChar(97);
-        RChar Cap[] = new RChar[5];
-//        System.out.println("Is this character a digit " + test.isDigit());
-//        System.out.println("Is this character a Letter " + test.isLetter());
-//        System.out.println("Give me ASCII of 97(a) to uppercase " + test.toUpperCase());
-//        System.out.println("Give me ASCII of 65(A) to lowercase " + test2.toLowerCase());
-//        System.out.println("size of array " + Cap.length);
-        for (int i = 0; i < Cap.length; i++) {
-            Cap[i] = new RChar(65 + i);
-        }
-
-        RString r = new RString("hfllo");
-        RString r2 = new RString("hello");
-        RString string = new RString(Cap);
-        System.out.println(string.hashCode());
-        System.out.println(Integer.MAX_VALUE);
-        System.out.println(r.compareTo(r2));
-        System.out.println(r.equals(r2));
+//        //System.out.print("MOE");
+////        RChar test = new RChar('s');
+////        RChar test2 = new RChar(97);
+//        RChar Cap[] = new RChar[5];
+////        System.out.println("Is this character a digit " + test.isDigit());
+////        System.out.println("Is this character a Letter " + test.isLetter());
+////        System.out.println("Give me ASCII of 97(a) to uppercase " + test.toUpperCase());
+////        System.out.println("Give me ASCII of 65(A) to lowercase " + test2.toLowerCase());
+////        System.out.println("size of array " + Cap.length);
+//        for (int i = 0; i < Cap.length; i++) {
+//            Cap[i] = new RChar(65 + i);
+//        }
+//
+//        RString r = new RString("hfllo");
+//        RString r2 = new RString("hello");
+//        RString string = new RString(Cap);
+//        System.out.println(string.hashCode());
+//        System.out.println(Integer.MAX_VALUE);
+//        System.out.println(r.compareTo(r2));
+//        System.out.println(r.equals(r2));
+//
+//        ArrayList<String> arrayList = new ArrayList();
+        RArrayList<String> arrayList = new RArrayList<String>();
+        arrayList.add("A");
+        arrayList.add("B");
+        arrayList.add("C");
+        arrayList.add("D");
+        arrayList.add("E");
+        arrayList.add("F");
+        arrayList.add("G");
+        System.out.println(arrayList.toString());
+        System.out.println("List Size is: " + arrayList.getListSize());
     }
 }
